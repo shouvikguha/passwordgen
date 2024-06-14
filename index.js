b79861e70fd3705f637606d547e20ebeade11b29
shouvikguha <sguha@ucsd.edu>
2024-06-14 02:33:05 -0700

    Copy text on click added and JS best practices for naming applied

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const gen_password_button = document.getElementById("generate-password-button");
-const passwords_element = document.getElementById("passwords");
-const passwords_arr = [];
-const password_length = document.querySelector(".password-length");
-const help_text = document.querySelector(".help-text");
+const genpasswordButton = document.getElementById("generate-password-button");
+const passwordsElement = document.getElementById("passwords");
+const passwordsArr = [];
+const passwordLength = document.querySelector(".password-length");
+const helpText = document.querySelector(".help-text");
+let isAlertShown = false; // flag to track if the notif has been shown
 
-password_length.addEventListener("input", function () {
-    const passwordLength = password_length.value;
-    if (passwordLength < 8 || passwordLength > 20){
-        help_text.style.display = "block";
+passwordLength.addEventListener("input", function () {
+    const passwordLengthValue = passwordLength.value;
+    if (passwordLengthValue < 8 || passwordLengthValue > 20) {
+        helpText.style.display = "block";
     }
     else {
-        help_text.style.display = "none";
+        helpText.style.display = "none";
     }
 });
 
 
-gen_password_button.addEventListener("click", function () {
+genpasswordButton.addEventListener("click", function () {
     var icon = document.getElementById("lock-icon");
     icon.classList.toggle("locked");
     setTimeout(function () {
@@ -29,14 +30,14 @@ gen_password_button.addEventListener("click", function () {
     }, 500);
 });
 
-function createPasswords(passwordLength) {
+function createPasswords(passwordLengthValue) {
 
-    const upper_case_letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    const lower_case_letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    const special_chars = ["@", "!", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", ":", ";", "~", "`", "[", "]", "?", ".", "-"];
+    const upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+    const lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    const specialChars = ["@", "!", "#", "$", "%", "^", "&", "*", "(", ")", "=", "+", ":", ";", "~", "`", "[", "]", "?", ".", "-", "<", ">"];
     const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    const all_letters = upper_case_letters.concat(lower_case_letters);
-    const special_chars_sub_dash_period = special_chars.filter(char => char !== "." && char !== "-");
+    const allLetters = upperCaseLetters.concat(lowerCaseLetters);
+    const specialcharsSubDashPeriod = specialChars.filter(char => char !== "." && char !== "-");
 
     // dash, period, shouldn"t be first; https://www.ibm.com/docs/en/baw/20.x?topic=security-characters-that-are-valid-user-ids-passwords
     // logic inspired by https://rublon.com/blog/nist-password-guidelines/
@@ -55,25 +56,25 @@ function createPasswords(passwordLength) {
     }
 
     const password = [];
-    const maxSpecialChars = Math.floor(passwordLength * 0.18);
-    const maxNumbers = Math.floor(passwordLength * 0.2);
+    const maxSpecialChars = Math.floor(passwordLengthValue * 0.18);
+    const maxNumbers = Math.floor(passwordLengthValue * 0.2);
     let specialCharCount = 0;
     let numberCount = 0;
-    password.push(getRandomElement(upper_case_letters));
-    password.push(getRandomElement(lower_case_letters));
+    password.push(getRandomElement(upperCaseLetters));
+    password.push(getRandomElement(lowerCaseLetters));
 
-    for (let i = 2; i < passwordLength; i++) {
+    for (let i = 2; i < passwordLengthValue; i++) {
         let char;
         if (specialCharCount < maxSpecialChars) {
-            char = getRandomElement(special_chars);
+            char = getRandomElement(specialChars);
             specialCharCount++;
-        } 
-        else if (numberCount < maxNumbers){
+        }
+        else if (numberCount < maxNumbers) {
             char = getRandomElement(numbers);
             numberCount++;
-        } 
-        else{
-            char = getRandomElement(all_letters);
+        }
+        else {
+            char = getRandomElement(allLetters);
         }
         password.push(char);
     }
@@ -81,8 +82,8 @@ function createPasswords(passwordLength) {
     shuffleArray(password);
 
     if (password[0] === "." || password[0] === "-") {
-        for (let i = 1; i < passwordLength; i++) {
-            if (special_chars_sub_dash_period.includes(password[i])) {
+        for (let i = 1; i < passwordLengthValue; i++) {
+            if (specialcharsSubDashPeriod.includes(password[i])) {
                 let temp = password[i];
                 password[i] = password[0];
                 password[0] = temp;
@@ -94,26 +95,42 @@ function createPasswords(passwordLength) {
     return password.join("");
 }
 
-function generatePasswords(shouldGenerate) {
-    if (shouldGenerate){
-        passwords_arr.length = 0;
-        passwords_element.innerHTML = "";
-        const passwordLength = parseInt(password_length.value);
 
-        if (passwordLength >= 8 && passwordLength <= 20) {
+
+
+function generatePasswords(shouldGenerate) {
+    if (shouldGenerate) {
+        passwordsArr.length = 0;
+        passwordsElement.innerHTML = "";
+        const passwordLengthValue = parseInt(passwordLength.value);
+
+        if (passwordLengthValue >= 8 && passwordLengthValue <= 20) {
             for (let i = 0; i < 4; i++) {
-                const newPassword = createPasswords(passwordLength);
-                if (newPassword.length === passwordLength) {
-                    passwords_arr.push(newPassword);
-                    passwords_element.innerHTML += `<li>${newPassword}</li>`;
-                } 
-                else{
-                    console.error("Generated password does not match desired length", newPassword);
+                const newPassword = createPasswords(passwordLengthValue);
+                if (newPassword.length === passwordLengthValue) {
+                    passwordsArr.push(newPassword);
+                    const listItem = document.createElement("li");
+                    listItem.textContent = newPassword;
+                    listItem.addEventListener('click', function () {
+                        navigator.clipboard.writeText(newPassword)
+                            .then(() => {
+                                if (!isAlertShown) { // limit to one alert
+                                    alert("Password copied to clipboard!");
+                                    isAlertShown = true;
+                                }
+                            })
+                            .catch(err => console.error("Failed to copy password: ", err));
+                    });
+                    passwordsElement.appendChild(listItem);
+                }
+                else {
+                    console.error("Password generated doesn't match intended length", newPassword);
                 }
             }
         }
-    } 
+    }
     else {
-        passwords_element.innerHTML = "";
+        passwordsElement.innerHTML = "";
+        isAlertShown = false;
     }
 }
